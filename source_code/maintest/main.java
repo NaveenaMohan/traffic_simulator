b44f8f1f949e39dcb52e7581f4fbd5ae0a2c7cdf
lorena <lorena.serna@kcl.ac.uk>
2015-02-12 20:44:22 +0000

    RoadNetwork -- hardcoding RUnits

---
+++ source_code/maintest/main.java
@@ -0,0 +1,15 @@
+package maintest;
+
+import com.sun.xml.internal.ws.api.pipe.Engine;
+import managers.roadnetwork.RoadNetwork;
+
+/**
+ * Created by Guera_000 on 12/02/2015.
+ */
+public class main {
+    public static void main(String[] args) {
+        //Engine engine= new Engine()
+
+        RoadNetwork rr = new RoadNetwork();
+    }
+}
